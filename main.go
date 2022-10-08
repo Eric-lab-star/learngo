5a6f30c690d0c1c73becd453506f1070aa161a93
eric-lab-star <cyon256@icloud.com>
2022-10-08 21:44:40 +0900

    fast url check

--- main.go
+++ main.go
@@ -2,17 +2,45 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	go sexyCount("kim")
-	sexyCount("Lee")
+type urlStruct struct {
+	url    string
+	status int
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
+func main() {
+	results := make(map[string]int)
+	channel := make(chan urlStruct)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
+
+	for _, url := range urls {
+		go hitUrl(url, channel)
+
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-channel
+		results[result.url] = result.status
+	}
+	for url, staus := range results {
+		fmt.Println(url, staus)
+	}
+
+}
+
+func hitUrl(url string, c chan<- urlStruct) {
+	resp, _ := http.Get(url)
+	c <- urlStruct{url: url, status: resp.StatusCode}
+
 }
