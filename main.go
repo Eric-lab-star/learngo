0edfc088f5cbbadcbc46b6b11d426bf7b854ee65
eric-lab-star <cyon256@icloud.com>
2022-10-09 23:21:55 +0900

    html query

--- main.go
+++ main.go
@@ -2,45 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
-type urlStruct struct {
-	url    string
-	status int
-}
+var baseURL string = "https://kr.indeed.com/jobs?q=javascript+react&limit=50&vjk=bf7bf53f892557fb"
 
 func main() {
-	results := make(map[string]int)
-	channel := make(chan urlStruct)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	for _, url := range urls {
-		go hitUrl(url, channel)
-
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-channel
-		results[result.url] = result.status
-	}
-	for url, staus := range results {
-		fmt.Println(url, staus)
-	}
+	getPages()
 
 }
 
-func hitUrl(url string, c chan<- urlStruct) {
-	resp, _ := http.Get(url)
-	c <- urlStruct{url: url, status: resp.StatusCode}
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkStatusCode(res)
 
+	body, err := io.ReadAll(res.Body)
+	checkErr(err)
+	fmt.Printf("%s", body)
+	defer res.Body.Close()
+	return pages
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+func checkStatusCode(resp *http.Response) {
+	if resp.StatusCode > 200 {
+		if resp.StatusCode != 403 {
+			log.Fatalln(resp.Status)
+		}
+	}
 }
